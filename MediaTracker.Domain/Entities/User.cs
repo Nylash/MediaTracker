53b8836e555c3104ae3601c4c8d81b49768e2f4d
Benjamin Peltier <ben.peltier7@gmail.com>
2026-02-16 16:31:20 +0100

    Add User entity

---
+++ MediaTracker.Domain/Entities/User.cs
@@ -0,0 +1,29 @@
+﻿namespace MediaTracker.Domain.Entities;
+
+public class User
+{
+    public Guid Id { get; private set; }
+
+    public string Username { get; private set; }
+
+    public string Email { get; private set; }
+
+    private User() { }
+
+    public User(string username, string email)
+    {
+        Id = Guid.NewGuid();
+        Username = username;
+        Email = email;
+    }
+
+    public void ChangeUsername(string newUsername)
+    {
+        Username = newUsername;
+    }
+
+    public void ChangeEmail(string newEmail)
+    {
+        Email = newEmail;
+    }
+}
