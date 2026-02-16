fef7af56c96c3ff9262fa2ce603e36375a346c8d
Benjamin Peltier <ben.peltier7@gmail.com>
2026-02-16 16:44:00 +0100

    Rename MediaList to UserList | Add entity UserListItem

---
+++ MediaTracker.Domain/Entities/UserList.cs
@@ -0,0 +1,24 @@
+﻿namespace MediaTracker.Domain.Entities;
+
+public class UserList
+{
+    public Guid Id { get; private set; }
+
+    public Guid UserId { get; private set; }
+
+    public string Name { get; private set; }
+
+    private UserList() { }
+
+    public UserList(Guid userId, string name)
+    {
+        Id = Guid.NewGuid();
+        UserId = userId;
+        Name = name;
+    }
+
+    public void Rename(string newName)
+    {
+        Name = newName;
+    }
+}
---
+++ MediaTracker.Domain/Entities/UserListItem.cs
@@ -0,0 +1,19 @@
+﻿namespace MediaTracker.Domain.Entities;
+
+public class UserListItem
+{
+    public Guid Id { get; private set; }
+
+    public Guid ListId { get; private set; }
+
+    public Guid MediaEntryId { get; private set; }
+
+    private UserListItem() { }
+
+    public UserListItem(Guid listId, Guid mediaEntryId)
+    {
+        Id = Guid.NewGuid();
+        ListId = listId;
+        MediaEntryId = mediaEntryId;
+    }
+}
