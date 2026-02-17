acbba364bf84d4c02e2fb6825056791ae1100361
Benjamin Peltier <ben.peltier7@gmail.com>
2026-02-17 09:26:16 +0100

    Add API basic configuration

--- MediaTracker.Api/Program.cs
+++ MediaTracker.Api/Program.cs
@@ -1,7 +1,17 @@
+using MediaTracker.Infrastructure.Persistence;
+using MediaTracker.Infrastructure.Repositories;
+using MediaTracker.Domain.Repositories;
+using MediaTracker.Domain.Services;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDbContext<MediaTrackerDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<IUserListRepository, UserListRepository>();
+builder.Services.AddScoped<IMediaEntryRepository, MediaEntryRepository>();
+builder.Services.AddScoped<UserListService>();
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
