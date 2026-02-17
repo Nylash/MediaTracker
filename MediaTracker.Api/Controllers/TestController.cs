496ed415d84e1ef62da92c63356e48e717674efd
Benjamin Peltier <ben.peltier7@gmail.com>
2026-02-17 10:25:36 +0100

    Add Swagger & TestController

---
+++ MediaTracker.Api/Controllers/TestController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MediaTracker.Domain.Services;
+
+namespace MediaTracker.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class TestController : ControllerBase
+{
+    private readonly UserListService _userListService;
+
+    public TestController(UserListService userListService)
+    {
+        _userListService = userListService;
+    }
+
+    [HttpGet]
+    public IActionResult Test()
+    {
+        return Ok("Injection OK");
+    }
+}
--- MediaTracker.Api/Program.cs
+++ MediaTracker.Api/Program.cs
@@ -13,15 +13,17 @@ builder.Services.AddScoped<IUserListRepository, UserListRepository>();
 builder.Services.AddScoped<IMediaEntryRepository, MediaEntryRepository>();
 builder.Services.AddScoped<UserListService>();
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+// Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
